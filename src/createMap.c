1c35d25332076bd05042f5f968ace9ac618f49f2
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2023-02-02 06:08:38 +0200

    I was working on generating of the game's world

--- src/createMap.c
+++ src/createMap.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 	FILE* map;
 	map = fopen("map.txt", "w");
+	
+	for(int i = 0; i < 10000; i++){
+		fputc('~', map);
+	}
+
+	fclose(map);
+	//int rn = (rand() % (12 - 1 + 1)) + 1; formula from webto get random numbers in given range
 
 	return 0;
 }
