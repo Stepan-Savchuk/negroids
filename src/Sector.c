875f5a5f6ad82ebc9748b46389acdb7d5902919f
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2024-02-10 16:24:06 +0200

    I have refactored reallocation function at Sector structure

--- src/Sector.c
+++ src/Sector.c
@@ -4,8 +4,8 @@ Sector* newSector(SectorID nid, int ncwood, int ncstone, int ncmetal, int nfland
 	//100 is aproximate value
 
 	int tBuildLimit = 100 / (ncwood + ncstone + ncmetal + nflands);
-	int tmetalBuildLimit = ncmetal / 100;
-	int tflandsBuildLimit = nflands / 100;
+	int tmetalBuildLimit = ncmetal / 10;
+	int tflandsBuildLimit = nflands / 10;
 
 
 	/*
@@ -59,10 +59,11 @@ void reallocBuildLimit(Sector* sector){
 	sector->buildLimit = 100 / (sector->cWood + sector->cStone + sector->cMetal + sector->flands);
 	sector->metalBuildLimit = sector->cMetal / 10;
 	sector->flandsBuildLimit = sector->flands / 10;
+  
+  reallocBuildingList(sector->buildings, sector->buildLimit);
+  reallocBuildingList(sector->metalBuildings, sector->metalBuildLimit);
+  reallocBuildingList(sector->flandsBuildings, sector->flandsBuildLimit);
 
-	sector->buildings = (BuildingList*) realloc(sector->buildings, sizeof(BuildingList) + sizeof(Building) * sector->buildLimit);
-	sector->metalBuildings = (BuildingList*) realloc(sector->metalBuildings, sizeof(BuildingList) + sizeof(Building) * sector->metalBuildLimit);
-	sector->flandsBuildings = (BuildingList*) realloc(sector->flandsBuildings, sizeof(BuildingList) + sizeof(Building) * sector->flandsBuildLimit);
 }
 
 void addBuildingS(Sector* sector, Building building){
