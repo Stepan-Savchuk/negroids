8768698a2f13eec54c4b9bc194b8e04d3179aaac
Stepan-Savchuk <stepan_savchuk3@protonmail.com>
2024-02-21 01:37:48 +0200

    Started on defining of Control functions(Inspired by Command pattern)

---
+++ include/Control.h
@@ -0,0 +1,26 @@
+#include "Player.h"
+#include "Slave.h"
+#include "Overseer.h"
+//TODO : Make Commands for controlling da shit
+
+void hitSlavePlayer(Player* player, Slave* slave);
+void hitSlaveOverseer(Overseer* overseer, Slave* slave);
+
+void shootSlavePlayer(Player* player, Slave* slave);
+void shootSlaveOverseer(Overseer* overseer, Slave* slave);
+
+bool checkPlayerLife(Player player);
+bool checkOverseerLife(Overseer overseer);
+bool checkSlaveLife(Slave slave);
+
+bool checkPlayerMental(Player player);
+bool checkOverseerMental(Overseer overseer);
+bool checkSlaveMental(Slave slave);
+
+//TODO : IN PROGRESS
+
+int getTotalProfit();
+
+void initNextDay();
+
+void shipArrival();
